2013170765fe0cc6f547c572860838f09f206c13
Mehrdad <MehSohayl@gmail.com>
2026-06-01 00:59:01 +0330

    feat(infrastructure): add initial database migrations

---
+++ PersonService.Infrastructure/Migrations/20260529205443_NationalCodeCorrection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NationalCodeCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName_Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName_Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalCode_Value = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    BirthDate_Value = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_IsDeleted",
+                table: "Persons",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_NationalCode_Value",
+                table: "Persons",
+                column: "NationalCode_Value",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ PersonService.Infrastructure/Migrations/20260529214336_ColumnsNameCorrection.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnsNameCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NationalCode_Value",
+                table: "Persons",
+                newName: "NationalCode");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName_Value",
+                table: "Persons",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName_Value",
+                table: "Persons",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "BirthDate_Value",
+                table: "Persons",
+                newName: "BirthDate");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Persons_NationalCode_Value",
+                table: "Persons",
+                newName: "IX_Persons_NationalCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NationalCode",
+                table: "Persons",
+                newName: "NationalCode_Value");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Persons",
+                newName: "LastName_Value");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Persons",
+                newName: "FirstName_Value");
+
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Persons",
+                newName: "BirthDate_Value");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Persons_NationalCode",
+                table: "Persons",
+                newName: "IX_Persons_NationalCode_Value");
+        }
+    }
+}
